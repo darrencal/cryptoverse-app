a0f2fd81e7bfea71961eedcaaf90bc06954c5c8b
Darren Cal <darrencal64@gmail.com>
2022-09-27 23:11:17 -0600

    Added content to crypto details page

---
+++ src/components/LineChart.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import { Col, Row, Typography } from "antd";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend
+);
+
+const { Title } = Typography;
+
+const LineChart = ({ coinHistory, currentPrice, coinName }) => {
+  const coinPrice = [];
+  const coinTimestamp = [];
+
+  for (let i = 0; i < coinHistory?.data?.history?.length; i++) {
+    coinPrice.unshift(coinHistory.data.history[i].price);
+    coinTimestamp.unshift(
+      new Date(
+        coinHistory.data.history[i].timestamp * 1000
+      ).toLocaleDateString()
+    );
+  }
+
+  const data = {
+    labels: coinTimestamp,
+    datasets: [
+      {
+        label: "Price in USD",
+        data: coinPrice,
+        fill: false,
+        backgroundColor: "#0071bd",
+        borderColor: "#0071bd",
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      yAxes: {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    },
+  };
+
+  return (
+    <>
+      <Row className="chart-header">
+        <Title level={2} className="chart-title">
+          {coinName} Price Chart
+        </Title>
+        <Col className="price-container">
+          <Title level={5} className="price-change">
+            {coinHistory?.data?.change}%
+          </Title>
+          <Title level={5} className="current-price">
+            Current {coinName} Price: ${currentPrice}
+          </Title>
+        </Col>
+      </Row>
+      <Line data={data} options={options} />
+    </>
+  );
+};
+
+export default LineChart;
